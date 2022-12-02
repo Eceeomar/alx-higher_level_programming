3e1c97041fd9c90ce4d3ea2b1476d05a178bc94a
Eceeomar <isiomaenuonye@gmail.com>
2022-12-02 11:31:54 -0800

    task 6 7 8 9 10

---
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -0,0 +1,60 @@
+#!/usr/bin/python3
+
+def print_matrix_integer(matrix=[[]]):
+    for row in matrix:
+        for column in row:
+            if column == row[-1]:
+                print('{:d}'.format(column), end='')
+            else:
+                print('{:d}'.format(column), end=' ')
+        print()
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+-- INSERT --                                                                                                                                                                  0,1           All
+
